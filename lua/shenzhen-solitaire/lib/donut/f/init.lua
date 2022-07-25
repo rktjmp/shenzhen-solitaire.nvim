be4525262a360abf88ae04435651595dd8714b34
Oliver Marriott <hello@omarriott.com>
2022-07-26 01:22:07 +1000

    Remove build artefacts

--- lua/shenzhen-solitaire/lib/donut/f/init.lua
+++
@@ -1,6 +0,0 @@
- local relrequire local function _1_(ddd) local function _2_(_241) return require(((string.match(ddd, "(.+%.)donut%.") or "") .. _241)) end return _2_ end relrequire = _1_(...)
-
- local function tap(x, f) f(x) return x end
- local function _then(x, f) return f(x) end
-
- return {tap = tap, ["then"] = _then}
